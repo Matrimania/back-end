7721663334c680c0634203e2e755605b1e88001d
Norma Lopez <norma_l304@yahoo.com>
2021-01-25 16:23:17 -0700

    Change debug to false

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@!w0u-pwfozn_f)(w8f)9ou(en7p6v)2!05e0c1&wn(oty2&&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
