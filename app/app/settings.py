1286ed619f9da954fc05bdd9ff62cbaff893379d
Norma Lopez <norma_l304@yahoo.com>
2021-01-25 16:37:29 -0700

    Test heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 DATABASES = {}
 URL = os.environ.get('DATABASE_URL', '')
